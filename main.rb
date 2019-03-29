8b0af75ee7a87856904916eaae169be0152cf86f
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-03-29 15:33:38 -0500

    [add] my inject, select, all?, any?, none?, count

--- main.rb
+++ main.rb
@@ -1,4 +1,7 @@
+# Enumerable Module extension
 module Enumerable
+  alias_method :my_reduce, :my_inject
+
   def my_each
     i = 0
     while i < length
@@ -17,36 +20,46 @@ module Enumerable
     self
   end
 
-  def my_reduce
-    p 'soy my reduce'
+  def my_inject(carry = self[0])
+    i = carry == self[0] ? 1 : 0
+    while i < length
+      carry = yield(carry, self[i])
+      i += 1
+    end
+    carry
   end
 
   def my_select
-    p 'soy my select'
+    selected = []
+    my_each do |i|
+      selected << i if yield(i)
+    end
+    selected
   end
 
   def my_all?
-    p 'soy my all?'
+    my_inject(true) { |res, i| res && yield(i) }
   end
 
   def my_any?
-    p 'soy my any?'
+    my_inject(false) { |res, i| res || yield(i) }
   end
 
   def my_none?
-    p 'soy my none?'
+    !my_any? { |i| yield(i) }
   end
 
-  def my_count
-    p 'soy my count'
+  def my_count(obj = nil)
+    count = 0
+    my_each do |i|
+      cond = block_given? ? yield(i) : (obj || i) == i
+      count += 1 if cond
+    end
+    count
   end
 
   def my_map
     p 'soy my map'
-  end
-
-  def my_inject
-    p 'soy my inject'
   end
 end
 
@@ -74,3 +87,66 @@ end
 # number: 4 index: 3
 # number: 5 index: 4
 # => [1, 2, 3, 4, 5]
+
+# ***** My Reduce *****
+# irb(main):007:0> numbers.reduce { |sum, n| sum + n }
+# => 15
+# irb(main):008:0> numbers.my_reduce { |sum, n| sum + n }
+# => 15
+
+# ***** My Select *****
+# irb(main):004:0> numbers.select { |n| n > 2 }
+# => [3, 4, 5]
+# irb(main):005:0> numbers.my_select { |n| n > 2 }
+# => [3, 4, 5]
+
+# ***** My All? *****
+# irb(main):016:0> numbers.all? { |n| n < 6 }
+# => true
+# irb(main):017:0> numbers.my_all? { |n| n < 6 }
+# => true
+# -----------------
+# irb(main):018:0> numbers.all? { |n| n < 4 }
+# => false
+# irb(main):019:0> numbers.my_all? { |n| n < 4 }
+# => false
+
+# ***** My Any? *****
+# irb(main):020:0> numbers.any? { |n| n == 4 }
+# => true
+# irb(main):021:0> numbers.my_any? { |n| n == 4 }
+# => true
+# -----------------
+# irb(main):022:0> numbers.any? { |n| n == 6 }
+# => false
+# irb(main):023:0> numbers.my_any? { |n| n == 6 }
+# => false
+
+# ***** My None? *****
+# irb(main):030:0> numbers.none? { |n| n > 6 }
+# => true
+# irb(main):031:0> numbers.my_none? { |n| n > 6 }
+# => true
+# -----------------
+# irb(main):032:0> numbers.none? { |n| n > 4 }
+# => false
+# irb(main):033:0> numbers.my_none? { |n| n > 4 }
+# => false
+
+# ***** My count *****
+# irb(main):049:0> numbers.count
+# => 5
+# irb(main):050:0> numbers.my_count
+# => 5
+# -----------------
+# irb(main):051:0> numbers.count(2)
+# => 1
+# irb(main):052:0> numbers.my_count(2)
+# => 1
+# -----------------
+# irb(main):054:0> numbers.count { |n| n <= 3 }
+# => 3
+# irb(main):055:0> numbers.my_count { |n| n <= 3 }
+# => 3
+
+# ***** My Map *****
